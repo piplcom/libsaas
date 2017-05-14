7eccc18ecab91c864c3c0581bace74c623e6462c
Yuri Ritvin <yuri.ritvin@pipl.com>
2017-05-14 10:52:48 +0300

    Updating api endpoint to search results by field

--- libsaas/services/pipedrive/service.py
+++ libsaas/services/pipedrive/service.py
@@ -343,7 +343,7 @@ class Pipedrive(base.Resource):
 
     @base.apimethod
     def search_by_field(self, term, field_type, field_key, exact_match=0,
-                        return_field_key=None, return_items_ids=1, start=None, limit=None):
+                        return_field_key=None, return_item_ids=1, start=None, limit=None):
         """
         Preform a search
         """
